c346c334060659b4eb1a22a5abb8be4d37f8f7b1
Sebastian Kootz <skxxtz@gmail.com>
2024-12-12 16:17:47 +0100

    improved power menu

--- rofi/scripts/power.sh
+++ rofi/scripts/power.sh
@@ -4,11 +4,11 @@ uptime="`uptime -p | sed -e 's/up //g'`"
 host=`hostname`
 
 # Options
-shutdown='⏻  Shutdown'
-reboot='󰑓  Reboot'
-lock='  Lock'
-suspend='󰏥  Suspend'
-logout='󰍃  Logout'
+shutdown='󰤆      Shutdown'
+reboot='      Reboot'
+lock='      Lock'
+suspend='󰤄      Suspend'
+logout='󰍃      Logout'
 yes=''
 no='󱊷'
 
