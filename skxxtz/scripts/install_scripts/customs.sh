3ea9edb8d8a9eb78a4caf4e71782d254c63e8060
Sebastian Kootz <skxxtz@gmail.com>
2025-01-15 13:39:35 +0100

    added apple emojis and fixed bug with wifi-font

--- skxxtz/scripts/install_scripts/customs.sh
+++ skxxtz/scripts/install_scripts/customs.sh
@@ -7,8 +7,20 @@ else
     echo "Creating OTF font directory..."
     sudo mkdir -p /usr/share/fonts/OTF/
 fi
+if [[ -d "/usr/share/fonts/TTF" ]]; then
+    echo "TTF directory already exists"
+else
+    echo "Creating TTF font directory..."
+    sudo mkdir -p /usr/share/fonts/OTF/
+fi
+
+echo "Copying 'wifi-font.otf into otf fonts...'"
 sudo cp ~/.config/skxxtz/fonts/wifi-font.otf /usr/share/fonts/OTF/
+echo "Copying 'SF-Pro-Display-Medium.otf' into otf fonts...'"
 sudo cp ~/.config/skxxtz/fonts/SF-Pro-Display-Medium.otf /usr/share/fonts/OTF/
+echo "Copying 'AppleColorEmoji.ttf' into ttf fonts...'"
+sudo cp ~/.config/skxxtz/fonts/AppleColorEmoji.ttf /usr/share/fonts/TTF/
+echo "Finished copying fonts."
 
 # Check if icon directory exists, if not, create it
 if [[ -d "/usr/share/icons" ]]; then
@@ -17,14 +29,21 @@ else
     echo "Creating OTF font directory..."
     sudo mkdir -p /usr/share/icons/
 fi
+echo "Copying 'macOS-White' into icons...'"
 sudo cp -r ~/.config/skxxtz/icons/macOS-White/ /usr/share/icons/
+echo "Finished copying icons."
+
+fc-cache -f -v
 
 
 rm -rf ~/.bashrc
-rm -rf ~/.bash_profile
-
-ln -s ~/.config/skxxtz/confs/bash_profile ~/.bash_profile
 ln -s ~/.config/skxxtz/confs/backrc ~/.bashrc
 
+rm -rf ~/.bash_profile
+ln -s ~/.config/skxxtz/confs/bash_profile ~/.bash_profile
 
-
+sudo rm -rf /etc/default/grub
+sudo ln -s ~/.config/skxxtz/confs/grub /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+sudo mkinitcpio -P
+sudo echo "options nvidia-drm modeset=1" > /etc/modprobe.d/nvidia.conf
--- skxxtz/scripts/install_scripts/gitconfiguration.sh
+++ skxxtz/scripts/install_scripts/gitconfiguration.sh
@@ -11,11 +11,10 @@ git config --global push.default simple
 git config --global core.editor "nvim"
 git config --global color.ui auto
 
-
+# Alias configurations
 git config --global alias.st status
 git config --global alias.co checkout
 git config --global alias.br branch
-git config --global alias.aa "add ."
-git config --global alias.cm "commit -m"
 
 git config --global --list
+
